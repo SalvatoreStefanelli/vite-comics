4b0b0a3f6b31f1af994145e0cc51739141032423
SalvatoreStefanelli <stefanellisalvatore.ss@gmail.com>
2024-01-31 22:10:46 +0100

    Initial commit. Create first component Header and send template of App in AppHeader

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+
+
+
+<template>
+  <AppHeader></AppHeader>
+
+  <main></main>
+  <footer></footer>
+</template>
+
+
+
+
+
+<style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,48 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <div class="logo">
+            <img src="/img/dc-logo.png" alt="">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+        </nav>
+
+    </header>
+</template>
+
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+}
+
+ul {
+    list-style: none;
+    display: flex;
+}
+
+& a {
+    text-decoration: none;
+    font-weight: bold;
+    padding: 0.5rem 1rem;
+    color: var(--comics-text-header);
+}
+</style>
